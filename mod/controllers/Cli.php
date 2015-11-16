afcd9059f88f4c4bb32bbbf6478216f65e9ed5d3
Fery Wardiyanto <ferywardiyanto@gmail.com>
2015-11-16 15:28:54 +0700

    Add basic console commands
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- mod/controllers/Cli.php
+++ mod/controllers/Cli.php
@@ -1,7 +1,23 @@
 <?php
+use Projek\CI\Common\Console;
 use Projek\CI\Console\Controller;
 
 class Cli extends Controller
 {
-    //
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->available_commands = [
+            Console\Install::class,
+            Console\Migration::class,
+        ];
+
+        foreach ($this->config->item('modules_available') as $name => $mod) {
+            if ($this->load->config($name.'/console', true, true)) {
+                $new_commands = $this->config->item($name.'/console', 'available_commands');
+                $this->available_commands = array_merge($this->available_commands, $new_commands);
+            }
+        }
+    }
 }
--- src/Module.php
+++ src/Module.php
@@ -42,6 +42,7 @@ class Module
             $location = rtrim($location, '/') . '/';
         }
 
+        $this->config->set_item('modules_available', $this->list);
         $this->config->set_item('modules_locations', $locations);
     }
 
